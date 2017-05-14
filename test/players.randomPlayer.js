b493c1770cf076cd194b6ac67bcfcf415393b9e2
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-05-13 17:54:13 -0700

    [prog] Write test for getting a random player

--- test/players.randomPlayer.js
+++ test/players.randomPlayer.js
@@ -3,8 +3,12 @@ const playerUtil = require('../server/utilities/playerUtils')
 
 describe('Random player', () => {
   it('should be returned when randomPlayer util invoked', () => {
-    playerUtil((err, data) => {
-      expect(err).to.be.a(null)
-    })
+    return playerUtil.randomPlayer()
+      .then(player => {
+        expect(player).to.be.ok
+        expect(player).to.have.ownProperty('name')
+        expect(player).to.have.ownProperty('startYear')
+        expect(player).to.have.ownProperty('endYear')
+      })
   })
 })
