856c541ea24817219eee683637702f2fc0376ab5
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-05-13 19:27:35 -0700

    [prog] Run test on the root of the problem

--- test/players.randomPlayer.js
+++ test/players.randomPlayer.js
@@ -1,71 +1,78 @@
 const expect = require('chai').expect
 const randomPlayer = require('../server/utilities/randomPlayerUtil')
+const nba = require('nba.js')
 
-describe('Random player', () => {
-  console.log('CAN I SEE THESE CONSOLESSS???')
-  it('should be returned when randomPlayer util invoked', done => {
-    console.log('ABOUT TO INVOKE GETTTT')
-    randomPlayer.get()
-      .then(player => {
-        expect(player).to.be.ok
-        expect(player).to.have.ownProperty('name')
-        expect(player).to.have.ownProperty('startYear')
-        expect(player).to.have.ownProperty('endYear')
-        done()
-      }).catch(err => {
-        console.log('errrrrrrr', err)
-        done()
-      })
-  }, 10000)
-})
+// describe('Random player', () => {
+//   console.log('CAN I SEE THESE CONSOLESSS???')
+//   it('should be returned when randomPlayer util invoked', done => {
+//     console.log('ABOUT TO INVOKE GETTTT')
+//     randomPlayer.get()
+//       .then(player => {
+//         expect(player).to.be.ok
+//         expect(player).to.have.ownProperty('name')
+//         expect(player).to.have.ownProperty('startYear')
+//         expect(player).to.have.ownProperty('endYear')
+//         done()
+//       }).catch(err => {
+//         console.log('errrrrrrr', err)
+//         done()
+//       })
+//   }, 10000)
+// })
 
 describe('NBA library', () => {
   it.only('should get every player that has ever played', () => {
-    console.log('CHANGING TEST TO REQUEST ALL PLAYERS')
-    return randomPlayer.requestAllPlayers()
-      .then(players => {
-        console.log('DOT THEN OF REQUEST ALL PLAYERS')
-        expect(players).to.be.ok
-        expect(players).to.be.instanceof(Array)
-        expect(players).to.have.lengthOf(4163)
-      })
+    console.log('LETS TRY TO SIMPLIFY THIS')
+    return nba.stats.allPlayers({ IsOnlyCurrentSeason: 0 }).then(data => {
+      console.log('OH SHIT WERE INSIDE THE DOT THEN')
+      return data.CommonAllPlayers
+    }).catch(err => {
+      console.log('WHAT WENT WRONG????', err)
+    })
+    // return randomPlayer.requestAllPlayers()
+    //   .then(players => {
+    //     console.log('DOT THEN OF REQUEST ALL PLAYERS')
+    //     expect(players).to.be.ok
+    //     expect(players).to.be.instanceof(Array)
+    //     expect(players).to.have.lengthOf(4163)
+    //   })
   }, 10000)
 })
 
-describe('Players Util', () => {
-  let players
+// describe('Players Util', () => {
+//   let players
 
-  before(() => {
-    return randomPlayer.requestAllPlayers()
-      .then(requestedPlayers => {
-        players = requestedPlayers
-      })
-  })
+//   before(() => {
+//     return randomPlayer.requestAllPlayers()
+//       .then(requestedPlayers => {
+//         players = requestedPlayers
+//       })
+//   })
 
-  it('should get every player that has ever played', () => {
-    const player = randomPlayer.pickRandomPlayerFromArray(players)
-    expect(player).to.be.ok
-    expect(player).to.be.an('object')
-    expect(players.includes(player)).to.be.true
-  })
-})
+//   it('should get every player that has ever played', () => {
+//     const player = randomPlayer.pickRandomPlayerFromArray(players)
+//     expect(player).to.be.ok
+//     expect(player).to.be.an('object')
+//     expect(players.includes(player)).to.be.true
+//   })
+// })
 
-describe('Players Util', () => {
-  let player
+// describe('Players Util', () => {
+//   let player
 
-  before(() => {
-    return randomPlayer.requestAllPlayers().then(players => randomPlayer.pickRandomPlayerFromArray(players))
-      .then(unformattedPlayer => {
-        player = unformattedPlayer
-      })
-  })
+//   before(() => {
+//     return randomPlayer.requestAllPlayers().then(players => randomPlayer.pickRandomPlayerFromArray(players))
+//       .then(unformattedPlayer => {
+//         player = unformattedPlayer
+//       })
+//   })
 
-  it('should get every player that has ever played', () => {
-    const formattedPlayer = randomPlayer.formatPlayer(player)
-    expect(formattedPlayer).to.be.ok
-    expect(formattedPlayer).to.be.an('object')
-    expect(formattedPlayer).to.have.ownProperty('name')
-    expect(formattedPlayer).to.have.ownProperty('startYear')
-    expect(formattedPlayer).to.have.ownProperty('endYear')
-  })
-})
+//   it('should get every player that has ever played', () => {
+//     const formattedPlayer = randomPlayer.formatPlayer(player)
+//     expect(formattedPlayer).to.be.ok
+//     expect(formattedPlayer).to.be.an('object')
+//     expect(formattedPlayer).to.have.ownProperty('name')
+//     expect(formattedPlayer).to.have.ownProperty('startYear')
+//     expect(formattedPlayer).to.have.ownProperty('endYear')
+//   })
+// })
