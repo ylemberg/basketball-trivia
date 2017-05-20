fe3a545cfadff5a989b068862035a417b993b41d
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-05-19 19:48:03 -0700

    [refactor] Change arrow notation to ES5

--- test/players.randomPlayer.spec.js
+++ test/players.randomPlayer.spec.js
@@ -2,16 +2,16 @@ const expect = require('chai').expect
 const randomPlayer = require('../server/utilities/randomPlayerUtil')
 const nba = require('nba.js')
 
-describe('Random player', () => {
+describe('Random player', function () {
   let player
-  before(() => {
+  before(function () {
     return randomPlayer.get()
       .then(result => {
         player = result
       })
   })
 
-  it('should be returned when randomPlayer util invoked', () => {
+  it('should be returned when randomPlayer util invoked', function () {
     expect(player).to.be.ok
     expect(player).to.have.ownProperty('name')
     expect(player).to.have.ownProperty('startYear')
@@ -19,32 +19,32 @@ describe('Random player', () => {
   })
 })
 
-describe('NBA library', () => {
+describe('NBA library', function () {
   let players
-  before(() => randomPlayer.requestAllPlayers()
+  before(function () randomPlayer.requestAllPlayers()
       .then(result => {
         players = result
       })
   )
 
-  it('should get every player that has ever played', () => {
+  it('should get every player that has ever played', function () {
     expect(players).to.be.ok
     expect(players).to.be.instanceof(Array)
     expect(players).to.have.lengthOf(4163)
   })
 })
 
-describe('Players Util', () => {
+describe('Players Util', function () {
   let players
 
-  before(() => {
+  before(function () {
     return randomPlayer.requestAllPlayers()
       .then(requestedPlayers => {
         players = requestedPlayers
       })
   })
 
-  it('should get every player that has ever played', () => {
+  it('should get every player that has ever played', function () {
     const player = randomPlayer.pickRandomPlayerFromArray(players)
     expect(player).to.be.ok
     expect(player).to.be.an('object')
@@ -52,17 +52,17 @@ describe('Players Util', () => {
   })
 })
 
-describe('Players Util', () => {
+describe('Players Util', function () {
   let player
 
-  before(() => {
+  before(function () {
     return randomPlayer.requestAllPlayers().then(players => randomPlayer.pickRandomPlayerFromArray(players))
       .then(unformattedPlayer => {
         player = unformattedPlayer
       })
   })
 
-  it('should get every player that has ever played', () => {
+  it('should get every player that has ever played', function () {
     const formattedPlayer = randomPlayer.formatPlayer(player)
     expect(formattedPlayer).to.be.ok
     expect(formattedPlayer).to.be.an('object')
