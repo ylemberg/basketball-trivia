4d926ab17af3df2b6e101b75c70d1844cb2a4def
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-07-08 10:42:25 -0700

    [refactor] React likes self closing tags when empty tags

--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -4,8 +4,7 @@ import { Button, Checkbox, Form, Grid, Image } from 'semantic-ui-react'
 const FormExampleForm = ({ player }) => (
   <Grid className="middle fit_to_parent_height" verticalAlign="middle" columns={4} centered>
     <Grid.Row>
-      <Grid.Column>
-      </Grid.Column>
+      <Grid.Column />
       <Grid.Column>
         <Form>
           <Form.Field>
@@ -13,11 +12,10 @@ const FormExampleForm = ({ player }) => (
             <label htmlFor="player_teammate">Player&#39;s Teammate</label>
             <input className="player_input" placeholder="Full Player Name" />
           </Form.Field>
-          <Button type="submit">Submit</Button>
+          <button className="ui inverted black button">GUESS PLAYER</button>
         </Form>
       </Grid.Column>
-      <Grid.Column>
-      </Grid.Column>
+      <Grid.Column />
     </Grid.Row>
   </Grid>
 )
